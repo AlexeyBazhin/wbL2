ab8084ef3e76c6b83609764fe24be5d213353758
AlexeyBazhin <alex.bajin1107@gmail.com>
2023-05-17 22:49:38 +0500

    dev2 done

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -19,5 +25,65 @@ package main
 */
 
 func main() {
+	result, err := repeatingRunes("a\\") // см. тесты
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
 
+func repeatingRunes(str string) (string, error) {
+	var result string
+
+	var curRune rune
+	var curCount int
+	var isEscape bool
+	for _, v := range str {
+		// если до этого встретили \, то запоминаем любую текущую руну
+		if isEscape {
+			isEscape = false
+			curRune = v
+			continue
+		}
+		// если встречаем цифры, то прибвляем к текущей сумме с учетом (прим.) e12 => eeeeeeeeeeee
+		if number, err := strconv.Atoi(string(v)); err == nil {
+			curCount = curCount*10 + number
+			continue
+		}
+
+		// перезаписываем строку на основе встреченной руны и ее повторений
+		var err error
+		if result, err = addToResult(result, curRune, curCount); err != nil {
+			return "", err
+		}
+
+		// обнуляемся, но учитываем \
+		curCount = 0
+		if v == rune('\\') {
+			isEscape = true
+			curRune = 0
+			continue
+		}
+		curRune = v
+	}
+	// если escape-последовательность была объявлена, но не реализована (прим.) "av\"
+	if isEscape {
+		return "", fmt.Errorf("invalid escape sequence")
+	}
+	return addToResult(result, curRune, curCount) // возвращаем строку (и ошибку) с последней повторенной n раз руной
+}
+
+func addToResult(result string, curRune rune, curCount int) (string, error) {
+	// если в начале встретилось число (прим.) "34av"
+	if curRune == 0 && curCount != 0 {
+		return "", fmt.Errorf("invalid string")
+	}
+	// если руна без повторений (должна повториться 1 раз)
+	if curRune != 0 && curCount == 0 {
+		curCount = 1
+	}
+	for i := 0; i < curCount; i++ {
+		result = strings.Join([]string{result, string(curRune)}, "")
+	}
+	return result, nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,124 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestWithoutRepeats(t *testing.T) {
+	str := "abcd"
+	newStr, err := repeatingRunes(str)
+	if err != nil {
+		t.Error(err)
+	}
+	if str != newStr {
+		t.Errorf("expected %s, got %s", str, newStr)
+	}
+}
+
+func TestWithoutRepeatsOneRune(t *testing.T) {
+	str := "a"
+	newStr, err := repeatingRunes(str)
+	if err != nil {
+		t.Error(err)
+	}
+	if str != newStr {
+		t.Errorf("expected %s, got %s", str, newStr)
+	}
+}
+
+func TestWithRepeats(t *testing.T) {
+	str := "a4bc2d5e12"
+	newStr, err := repeatingRunes(str)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedStr := "aaaabccdddddeeeeeeeeeeee"
+	if expectedStr != newStr {
+		t.Errorf("expected %s, got %s", expectedStr, newStr)
+	}
+}
+
+func TestInvalidString(t *testing.T) {
+	if _, err := repeatingRunes("32534"); err == nil {
+		t.Error("expected error")
+	} else if err.Error() != "invalid string" {
+		t.Error("another error")
+	}
+}
+
+func TestInvalidStringOneRune(t *testing.T) {
+	if _, err := repeatingRunes("3"); err == nil {
+		t.Error("expected error")
+	} else if err.Error() != "invalid string" {
+		t.Error("another error")
+	}
+}
+
+func TestWithEscape(t *testing.T) {
+	str := "a\\4"
+	newStr, err := repeatingRunes(str)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedStr := "a4"
+	if expectedStr != newStr {
+		t.Errorf("expected %s, got %s", expectedStr, newStr)
+	}
+}
+
+func TestWithEscapes(t *testing.T) {
+	str := "a\\4\\5\\6"
+	newStr, err := repeatingRunes(str)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedStr := "a456"
+	if expectedStr != newStr {
+		t.Errorf("expected %s, got %s", expectedStr, newStr)
+	}
+}
+
+func TestWithEscapesWithRepeats(t *testing.T) {
+	str := "a\\45\\53\\62"
+	newStr, err := repeatingRunes(str)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedStr := "a4444455566"
+	if expectedStr != newStr {
+		t.Errorf("expected %s, got %s", expectedStr, newStr)
+	}
+}
+
+func TestWithSlashEscape(t *testing.T) {
+	str := "a\\\\3" // a\\3
+	newStr, err := repeatingRunes(str)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedStr := "a\\\\\\" // a\\\
+	if expectedStr != newStr {
+		t.Errorf("expected %s, got %s", expectedStr, newStr)
+	}
+	// t.Log(newStr)
+}
+
+func TestWithInvalidEscape(t *testing.T) {
+	if _, err := repeatingRunes("a\\"); err == nil {
+		t.Error("expected error")
+	} else if err.Error() != "invalid escape sequence" {
+		t.Error("another error")
+	}
+}
+
+func TestEmptyString(t *testing.T) {
+	str := ""
+	newStr, err := repeatingRunes(str)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedStr := ""
+	if expectedStr != newStr {
+		t.Errorf("expected %s, got %s", expectedStr, newStr)
+	}
+}
