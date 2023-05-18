a617fb5a6144cbc21c8e26ac48ed97865addea29
AlexeyBazhin <alex.bajin1107@gmail.com>
2023-05-18 15:51:09 +0500

    dev3 done

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,21 +1,31 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита sort ===
 
 Отсортировать строки (man sort)
 Основное
 
-Поддержать ключи
+# Поддержать ключи
 
 -k — указание колонки для сортировки
 -n — сортировать по числовому значению
 -r — сортировать в обратном порядке
 -u — не выводить повторяющиеся строки
 
-Дополнительное
+# Дополнительное
 
-Поддержать ключи
+# Поддержать ключи
 
 -M — сортировать по названию месяца
 -b — игнорировать хвостовые пробелы
@@ -24,7 +34,137 @@ package main
 
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
+type SortOptions struct {
+	column  int  // -k
+	numeric bool // -n
+	reverse bool // -r
+	unique  bool // -u
+}
+
+func ParseFlags() *SortOptions {
+	options := &SortOptions{}
+
+	// Определение флагов командной строки
+	flag.IntVar(&options.column, "k", 1, "Указание колонки для сортировки")
+	flag.BoolVar(&options.numeric, "n", false, "Сортировать по числовому значению")
+	flag.BoolVar(&options.reverse, "r", false, "Сортировать в обратном порядке")
+	flag.BoolVar(&options.unique, "u", false, "Не выводить повторяющиеся строки")
+
+	// Парсинг аргументов командной строки
+	flag.Parse()
+
+	return options
+}
+
+func ReadFile(filename string) ([]string, error) {
+	strs := make([]string, 0)
+	if filename != "" {
+		file, err := os.Open(filename)
+		if err != nil {
+			return strs, err
+		}
+		defer file.Close()
+
+		buf := bufio.NewScanner(file)
+		for buf.Scan() {
+			strs = append(strs, buf.Text())
+		}
+		if err := buf.Err(); err != nil {
+			return strs, err
+		}
+	}
+	return strs, nil
+}
+
+func WriteFile(filename string, strs []string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	for _, v := range strs {
+		_, err = file.WriteString(v + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func main() {
+	options := ParseFlags()
 
+	strs, err := ReadFile(flag.Arg(0))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error reading file: err:", err)
+		os.Exit(1)
+	}
+
+	strs, err = SortStringsInFile(strs, options)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error sorting: err:", err)
+		os.Exit(1)
+	}
+
+	if err := WriteFile(flag.Arg(1), strs); err != nil {
+		fmt.Fprintln(os.Stderr, "error writing file: err:", err)
+		os.Exit(1)
+	}
+}
+
+func SortStringsInFile(strs []string, options *SortOptions) ([]string, error) {
+	if options.unique {
+		strs = MakeUnique(strs)
+	}
+	// с сохранением изначального порядка равных элементов
+	sort.SliceStable(strs, func(i, j int) bool {
+		if strings.TrimSpace(strs[i]) == "" {
+			return false
+		}
+		iColumns := strings.Split(strs[i], " ")
+		jColumns := strings.Split(strs[j], " ")
+		// если у первого числа не существует колонки, по которой идет сортировка (по дефолту сортируется по 1)
+		// то тогда оно будет больше и вставляться в конец (если сравнивать с другими пустыми, то порядок сохраняется)
+		if len(iColumns) < options.column {
+			return false
+		}
+		// аналогично
+		if len(jColumns) < options.column {
+			return true
+		}
+
+		if options.numeric {
+			iNum, iErr := strconv.Atoi(iColumns[options.column-1])
+			jNum, jErr := strconv.Atoi(jColumns[options.column-1])
+			if iErr != nil {
+				return false // не удалось запарсить - отправляем в конец
+			}
+			if jErr != nil {
+				return true
+			}
+			return iNum < jNum
+		}
+		result := iColumns[options.column-1] < jColumns[options.column-1]
+		return result
+	})
+
+	if options.reverse {
+		for i, j := 0, len(strs)-1; i < j; i, j = i+1, j-1 {
+			strs[i], strs[j] = strs[j], strs[i]
+		}
+	}
+
+	return strs, nil
+}
+
+func MakeUnique(strs []string) []string {
+	unique := make(map[string]struct{})
+	newStrs := []string{}
+	for _, str := range strs {
+		if _, ok := unique[str]; !ok {
+			unique[str] = struct{}{}
+			newStrs = append(newStrs, str)
+		}
+	}
+	return newStrs
 }
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,61 @@
 package main
+
+import "testing"
+
+var tests = []struct {
+	name    string
+	options *SortOptions
+	input   []string
+	output  []string
+}{
+	{"simple_test", &SortOptions{1, false, false, false}, []string{"cab 123", "asz 123", "azf 123"}, []string{"asz 123", "azf 123", "cab 123"}},
+	{"reverse_test", &SortOptions{1, false, true, false}, []string{"cab 123", "asz 123", "azf 123"}, []string{"cab 123", "azf 123", "asz 123"}},
+	{"with_invalid_columns_test", &SortOptions{2, false, false, false}, []string{"cab 1234", "asz", "azf 23", "b"}, []string{"cab 1234", "azf 23", "asz", "b"}},
+	{"numeric_test", &SortOptions{2, true, false, false}, []string{"cab 1234", "asz", "azf 23", "b"}, []string{"azf 23", "cab 1234", "asz", "b"}},
+	{"numeric_test_reverse", &SortOptions{2, true, true, false}, []string{"cab 1234", "asz", "azf 23", "b"}, []string{"b", "asz", "cab 1234", "azf 23"}},
+	{"unique_test", &SortOptions{1, false, false, true}, []string{"c", "a", "d", "c", "a", "d"}, []string{"a", "c", "d"}},
+}
+
+func isStringsEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestSortRun(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			strs, _ := SortStringsInFile(test.input, test.options)
+			if !isStringsEqual(strs, test.output) {
+				t.Fatalf("%v - output is not expected: want: %#v have: %#v", test.name, test.output, strs)
+			}
+		})
+	}
+}
+
+// func TestSimple(t *testing.T) {
+// 	strs, _ := SortStringsInFile(tests[0].input, tests[0].options)
+// 	if !isStringsEqual(strs, tests[0].output) {
+// 		t.Fatalf("output is not expected: want: %+#v have: %+#v", tests[0].output, strs)
+// 	}
+// }
+
+// func TestReverse(t *testing.T) {
+// 	strs, _ := SortStringsInFile(tests[1].input, tests[1].options)
+// 	if !isStringsEqual(strs, tests[1].output) {
+// 		t.Fatalf("output is not expected: want: %+#v have: %+#v", tests[1].output, strs)
+// 	}
+// }
+
+// func TestWithInvalidColumns(t *testing.T) {
+// 	strs, _ := SortStringsInFile(tests[2].input, tests[2].options)
+// 	if !isStringsEqual(strs, tests[2].output) {
+// 		t.Fatalf("output is not expected: want: %+#v have: %+#v", tests[2].output, strs)
+// 	}
+// }
